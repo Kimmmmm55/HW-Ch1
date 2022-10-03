4e126d5acdb1a4e569f36493e2874f0996840e18
Kimmmmm55 <peggysoso83166@gmail.com>
2022-10-03 21:52:45 +0800

    HW-ch1

---
+++ test_p16/src/p16/p16.java
@@ -0,0 +1,13 @@
+package p16;
+
+public class p16 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println('A');
+		System.out.println("歡迎使用java!");
+		System.out.print(123);
+	}
+
+}
